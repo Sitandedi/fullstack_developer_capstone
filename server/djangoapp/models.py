422259eea3fd2bafff101369c6ba620aa24a78d5
Sitandedi <f.nyamsi@gmail.com>
2025-10-21 19:44:00 -0400

    message

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -35,6 +35,8 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+"""
+
 class CarModel(models.Model):
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
     name = models.CharField(max_length=100)
@@ -50,11 +52,33 @@ class CarModel(models.Model):
     
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023, validators=[MaxValueValidator(2026), MinValueValidator(2015)])
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True)
     image = models.ImageField(upload_to='car_models/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.make.name} {self.name} ({self.year})"
+
+"""
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2026),
+            MinValueValidator(2015)
+        ])
+    # Other fields as needed
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
